a9d759b8541bbe03cafe528367e96c2e755a436b
andrewliaskovski <andrewliaskovski@gmail.com>
2018-12-18 10:43:53 +0100

    Update Main.java

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -1,20 +1,12 @@
 package HelloWorld;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world");
+public class Main {
+  public static void main(String[] args {
+    boolean flag = true;
+    System.out.println("Hello world");
+    while(flag != false) { 
+      System.out.println("I'm alive!");
+      Thread.sleep(1000);
     }
-
-
-
-boolean flag = true;
-System.out.println("Hello world");
-while(flag != false) { 
-
-System.out.println("I'm alive!");
-Thread.sleep(1000);
-}
-
+  }
 }
