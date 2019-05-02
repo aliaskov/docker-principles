863b77cfdc931cfa481781000c5cacae0e177cc3
admin <admin@MacBookKastl.local>
2019-05-02 17:44:43 +0200

    added few lines

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         boolean flag = true;
         System.out.println("Hello world");
+        System.out.println("Hello from Andrew");
+        System.out.println("Hello again");
         while(flag != false) {
             System.out.println("I'm alive!");
             Thread.sleep(1000);
