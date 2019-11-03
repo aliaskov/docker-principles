f262eda6cdedd285b8e3423b092941167c284f6a
x3matt <49571785+x3matt@users.noreply.github.com>
2019-11-03 20:40:06 +0700

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -6,12 +6,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world");
         System.out.println("Hello from Andrew");
-        System.out.println("Hello again@");
         System.out.println("Hello again");
         while(true) {
             System.out.println("["+LocalDateTime.now() +"] :"+"I'm alive!");
             Thread.sleep(1000);
         }
-	System.out.println("Hello world");
     }
 }
